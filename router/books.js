fd68e8da533160306662f56dea6f29dae24b018d
Himanshu Bhoir <himanshubhoir25@gmail.com>
2023-06-19 13:34:14 +0530

    prj

---
+++ router/books.js
@@ -0,0 +1,11 @@
+let books = {
+    1: {"author": "author name 1","title": "book name 1", "reviews": {} },
+    2: {"author": "author name 2","title": "book name 2", "reviews": {} },
+    3: {"author": "author name 3","title": "book name 3", "reviews": {} },
+    4: {"author": "author name 4","title": "book name 4", "reviews": {} },
+    5: {"author": "author name 5","title": "book name 5", "reviews": {} },
+    6: {"author": "author name 6","title": "book name 6", "reviews": {} },
+    7: {"author": "author name 7","title": "book name 7", "reviews": {} },
+}
+
+module.exports=books;
